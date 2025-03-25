5fd9651ea55a8f62df719008b278980441e1aecb
Julien Brayere <julien.brayere@obitrain.com>
2025-03-25 11:55:34 +0100

    chore: chore wip

--- tests/migration/test_utils.py
+++ tests/migration/test_utils.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import pathlib
 
 import pytest
 
@@ -12,52 +13,123 @@ def migration_folder(tmp_path):
     return tmp_path
 
 
-@pytest.fixture()
-def setup_migration_folder(migration_folder):
-    files = [
-        ("0000000", DEFAULT_TIME),
-        ("0000001", DEFAULT_TIME),
-        ("0000002", DEFAULT_TIME),
-        ("0000003", DEFAULT_TIME),
-        ("0000004", DEFAULT_TIME),
-    ]
+class TestReorderByMigrations:
+    @pytest.fixture()
+    def setup_migration_folder(self, migration_folder):
+        files = [
+            ("0000000", DEFAULT_TIME),
+            ("0000001", DEFAULT_TIME),
+            ("0000002", DEFAULT_TIME),
+            ("0000003", DEFAULT_TIME),
+            ("0000004", DEFAULT_TIME),
+        ]
 
-    _prev_file_up, _prev_file_down = None, None
-    for _file_id, _file_ts in files:
-        _base_name = MigrationFile.generate_base_name(ts=int(_file_ts.timestamp()), file_id=_file_id)
-        for _file_type in ["up", "down"]:
-            _prev_file = _prev_file_up if _file_type == "up" else _prev_file_down
-            _file = MigrationFile(
-                ts=_file_ts,
-                file_id=_file_id,
-                file_type=_file_type,
-                path=migration_folder / f"{_base_name}-{_file_type}.sql",
-                header=Header(prev_file=_prev_file, author="foo", version=None),
-            )
-            _file.write_header()
-            if _file_type == "up":
-                _prev_file_up = _file.name
-            else:
-                _prev_file_down = _file.name
+        _prev_file_up, _prev_file_down = None, None
+        for _file_id, _file_ts in files:
+            _base_name = MigrationFile.generate_base_name(ts=int(_file_ts.timestamp()), file_id=_file_id)
+            for _file_type in ["up", "down"]:
+                _prev_file = _prev_file_up if _file_type == "up" else _prev_file_down
+                _file = MigrationFile(
+                    ts=_file_ts,
+                    file_id=_file_id,
+                    file_type=_file_type,
+                    path=migration_folder / f"{_base_name}-{_file_type}.sql",
+                    header=Header(prev_file=_prev_file, author="foo", version=None),
+                )
+                _file.write_header()
+                if _file_type == "up":
+                    _prev_file_up = _file.name
+                else:
+                    _prev_file_down = _file.name
+
+    _empty_folder = pytest.param(None, [], ["0000000", "0000001", "0000002", "0000003", "0000004"], id="empty folder")
+
+    _one_file_change = pytest.param(
+        None, ["0000002", "0000004"], ["0000000", "0000001", "0000004", "0000002", "0000003"], id="one file change"
+    )
+
+    @pytest.mark.usefixtures("setup_migration_folder")
+    @pytest.mark.parametrize("setup_fn, last_migration_ids, expected", [_empty_folder, _one_file_change])
+    def test_reorder(self, setup_fn, last_migration_ids, migration_folder, expected):
+        from padmy.migration import reorder_files_by_migrations, verify_migration_files
+
+        if setup_fn is not None:
+            setup_fn()
+
+        reorder_files_by_migrations(migration_folder, last_migration_ids=last_migration_ids)
+
+        new_order = list([x.file_id for x in get_files(migration_folder, up_only=True)])
+        assert new_order == expected
+        assert not verify_migration_files(migration_folder)
 
 
-_empty_folder = pytest.param(None, [], ["0000000", "0000001", "0000002", "0000003", "0000004"], id="empty folder")
+class TestReorder:
+    @staticmethod
+    def _get_valid_order(folder: pathlib.Path):
+        for _file_id, _header in [
+            ("0000000", Header(prev_file=None, author="foo", version=None)),
+            ("0000001", Header(prev_file="0000000", author="foo", version=None)),
+            ("0000002", Header(prev_file="0000001", author="foo", version=None)),
+        ]:
+            _base_name = MigrationFile.generate_base_name(ts=int(DEFAULT_TIME.timestamp()), file_id=_file_id)
+            for _file_type in ["up", "down"]:
+                _file = MigrationFile(
+                    ts=DEFAULT_TIME,
+                    file_id=_file_id,
+                    file_type=_file_type,
+                    path=folder / f"{_base_name}-{_file_type}.sql",
+                    header=_header,
+                )
+                _file.write_header()
+                if _file_type == "up":
+                    _prev_file_up = _file.name
+                else:
+                    _prev_file_down = _file.name
 
-_one_file_change = pytest.param(
-    None, ["0000002", "0000004"], ["0000000", "0000001", "0000004", "0000002", "0000003"], id="one file change"
-)
+    @staticmethod
+    def _get_invalid_header(folder: pathlib.Path):
+        for _file_id, _header in [
+            ("0000000", Header(prev_file=None, author="foo", version=None)),
+            ("0000001", Header(prev_file="0000001", author="foo", version=None)),
+            ("0000002", Header(prev_file="0000001", author="foo", version=None)),
+        ]:
+            _base_name = MigrationFile.generate_base_name(ts=int(DEFAULT_TIME.timestamp()), file_id=_file_id)
+            for _file_type in ["up", "down"]:
+                _file = MigrationFile(
+                    ts=DEFAULT_TIME,
+                    file_id=_file_id,
+                    file_type=_file_type,
+                    path=folder / f"{_base_name}-{_file_type}.sql",
+                    header=_header,
+                )
+                _file.write_header()
+                if _file_type == "up":
+                    _prev_file_up = _file.name
+                else:
+                    _prev_file_down = _file.name
 
+    _valid = pytest.param(
+        lambda folder: TestReorder._get_valid_order(folder),
+        ["0000000", "0000001", "0000002"],
+        id="valid order",
+    )
 
-@pytest.mark.usefixtures("setup_migration_folder")
-@pytest.mark.parametrize("setup_fn, last_migration_ids, expected", [_empty_folder, _one_file_change])
-def test_reorder(setup_fn, last_migration_ids, migration_folder, expected):
-    from padmy.migration import reorder_files, verify_migration_files
+    _invalid_header_order = pytest.param(
+        lambda folder: TestReorder._get_invalid_header(folder),
+        ["0000000", "0000001", "0000002"],
+        id="invalid header order",
+    )
 
-    if setup_fn is not None:
-        setup_fn()
+    # @pytest.mark.usefixtures("setup_migration_folder")
+    @pytest.mark.parametrize("setup_fn, expected", [_valid, _invalid_header_order])
+    def test_reorder(self, setup_fn, migration_folder, expected):
+        from padmy.migration import reorder_files, verify_migration_files
 
-    reorder_files(migration_folder, last_migration_ids=last_migration_ids)
+        if setup_fn is not None:
+            setup_fn(migration_folder)
 
-    new_order = list([x.file_id for x in get_files(migration_folder, up_only=True)])
-    assert new_order == expected
-    assert not verify_migration_files(migration_folder)
+        reorder_files(migration_folder)
+
+        new_order = list([x.file_id for x in get_files(migration_folder, up_only=True)])
+        assert new_order == expected
+        assert not verify_migration_files(migration_folder)
